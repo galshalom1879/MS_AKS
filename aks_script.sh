ecebda025873c960c866b9f830ec8995ad0ac4c2
unknown <galshalo@post.bgu.ac.il>
2024-04-15 01:11:21 +0300

    :)

--- aks_script.sh
+++ aks_script.sh
@@ -7,8 +7,8 @@ aksClusterName="gs-cluster-1"
 acrName="galshaacr"
 
 # Set the subscription
-subscriptionID="paste here your subscription ID"
-az account set --subscription "$subscriptionID"
+# subscriptionID="paste here your subscription ID"
+# az account set --subscription "$subscriptionID"
 
 # Create a resource group
 az group create --name $resourceGroupName --location $location
@@ -36,6 +36,7 @@ az deployment group create \
 # get access to cluster using Kubectl
 az aks get-credentials --resource-group $resourceGroupName --name $aksClusterName
 kubectl config use-context $aksClusterName
+export KUBECONFIG=/mnt/c/Users/Rachel/.kube/config
 
 # install ingress controller
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginxq
@@ -47,4 +48,15 @@ helm install my-nginx ingress-nginx/ingress-nginx \
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
 echo "Ingress installed"
 
-kubectl apply -f ingress.yaml -f k8s/NetworkPolicy.yaml -f k8s/service-a-deployment.yaml -f k8s/service-a-service.yaml -f k8s/service-b-deployment.yaml -f k8s/service-b-service.yaml 
+# Assign owner role for cluster+agentpool within the scope of the ACR
+clusterID=$(az aks show --resource-group "$resourceGroupName" --name $aksClusterName --query identity.principalId -o tsv)
+agentpoolID=$(az aks show --resource-group "$resourceGroupName" --name $aksClusterName --query identityProfile.kubeletidentity.objectId -o tsv)
+roleScope=$(az acr show --name $acrName --resource-group $resourceGroupName --query id --output tsv)
+
+clusterID=$(echo "$clusterID" | tr -d '\r')
+roleScope=$(echo "$roleScope" | tr -d '\r')
+
+az role assignment create --assignee-object-id $clusterID --assignee-principal-type ServicePrincipal --role "Owner" --scope $roleScope
+az role assignment create --assignee-object-id $agentpoolID --assignee-principal-type ServicePrincipal --role "Owner" --scope $roleScope
+
+kubectl apply -f k8s/ingress.yaml -f k8s/NetworkPolicy.yaml -f k8s/service-a-deployment.yaml -f k8s/service-a-service.yaml -f k8s/service-b-deployment.yaml -f k8s/service-b-service.yaml 
